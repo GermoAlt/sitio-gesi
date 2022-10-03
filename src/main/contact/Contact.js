b6dfed9d84e388fd8a1747a906b6b72a14d44a11
German <germanaltairac@hotmail.com>
2022-10-03 17:11:43 -0300

    Contact WIP

--- src/main/contact/Contact.js
+++ src/main/contact/Contact.js
@@ -3,8 +3,21 @@ import './contact.css'
 export default function Contact() {
     return (
         <div id={"contacto"}>
-            <input/>
-            <textarea></textarea>
+            <div className={"personal-info-container"}>
+                <div>
+                    <label htmlFor={"contact-name"}>Nombre</label>
+                    <input id={"contact-name"}/>
+                </div>
+                <div>
+                    <label htmlFor={"contact-email"}>Mail</label>
+                    <input id={"contact-email"}/>
+                </div>
+            </div>
+            <label htmlFor={"contact-message"}>Mensaje</label>
+            <textarea id={"contact-message"}></textarea>
+            <div className={"button-container"}>
+                <button>ENVIAR</button>
+            </div>
         </div>
     )
 }
