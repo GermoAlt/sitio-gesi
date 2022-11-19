54481c5761c5408976d220270c146870b7b95c62
German <germanaltairac@hotmail.com>
2022-11-19 00:50:22 -0300

    primereact dependency

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";     
+import "primereact/resources/themes/saga-blue/theme.css"   
 import Header from "./header/Header";
 import Main from "./main/Main";
 import Footer from "./footer/Footer";
