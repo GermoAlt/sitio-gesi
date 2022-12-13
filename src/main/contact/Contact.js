b850770eb8ea3f9344eecb097b374d869d940374
Germán Altairac <germanaltairac@hotmail.com>
2022-12-12 23:50:41 -0300

    Contact form

--- src/main/contact/Contact.js
+++ src/main/contact/Contact.js
@@ -1,33 +1,54 @@
 import './contact.css'
-import {useState} from "react";
+import {useRef, useState} from "react";
+import emailjs from '@emailjs/browser';
+import {Toast} from "primereact/toast";
 
 export default function Contact() {
-    const [captcha, setCaptcha] = useState(false)
     const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [message, setMessage] = useState("")
+    const form = useRef()
+    const toast = useRef()
 
-    const emailHandler = ()=> {
-        // if(!captcha || !name || !email || !message) return;
-        window.open("mailto:germanaltairac@hotmail.com;german98altairac@gmail.com?subject=Consulta GESI&body=hola")
+    const emailHandler = (e)=> {
+        e.preventDefault()
+        if(!name || !email || !message) {
+            toast.current.show({severity: 'error', summary: 'Complete los campos', detail: 'Debe llenar los tres campos con su información', life:5000});
+            return;
+        }
+        emailjs.sendForm("service_hmjvhzf", "template_8zvv4nb", form.current, "Q3Yn66ekxY4qIEOKS")
+            .then((res)=>{
+                toast.current.show({severity: 'success', summary: 'Mensaje enviado!', detail: 'Tu mensaje fue enviado con éxito', sticky:true});
+                cleanFields()
+            }, (error)=> {
+                toast.current.show({severity: 'error', summary: 'Error de sistema', detail: 'Ocurrió un error durante el envío de tu mensaje. Esperá unos minutos e intentá de nuevo, o contactanos via Facebook', sticky:true});
+            })
     }
+
+    const cleanFields = () => {
+        setMessage("")
+        setEmail("")
+        setName("")
+    }
+
     return (
-        <div id={"contacto"}>
+        <form id={"contacto"} ref={form} onSubmit={emailHandler}>
+            <Toast ref={toast}/>
             <div className={"personal-info-container"}>
                 <div>
-                    <label htmlFor={"contact-name"}>Nombre</label>
-                    <input id={"contact-name"}/>
+                    <label htmlFor={"contact-name"}>Nombre*</label>
+                    <input id={"contact-name"} name={"user_name"} value={name} onChange={(e)=>setName(e.target.value)}/>
                 </div>
                 <div>
-                    <label htmlFor={"contact-email"}>Mail</label>
-                    <input id={"contact-email"}/>
+                    <label htmlFor={"contact-email"}>Mail*</label>
+                    <input id={"contact-email"} name={"user_email"} value={email} onChange={(e)=>setEmail(e.target.value)}/>
                 </div>
             </div>
-            <label htmlFor={"contact-message"}>Mensaje</label>
-            <textarea id={"contact-message"}></textarea>
+            <label htmlFor={"contact-message"}>Mensaje*</label>
+            <textarea id={"contact-message"} name={"message"} value={message} onChange={(e)=>setMessage(e.target.value)}/>
             <div className={"button-container"}>
-                <button onClick={()=>emailHandler()}>ENVIAR</button>
+                <input type={"submit"} value={"ENVIAR"}/>
             </div>
-        </div>
+        </form>
     )
 }
